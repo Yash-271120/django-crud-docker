3e34b7750d687c2b7390ff39cbac8c0abc337e0c
Yash-271120 <yashpatil.yp2711@gmail.com>
2024-06-12 13:14:17 +0530

    add: db seed script

---
+++ test/src/scripts/seed.js
@@ -0,0 +1,17 @@
+db.todos.insertMany([
+    {
+        content:"Go to the gym"
+    },
+    {
+        content:"Buy groceries"
+    },
+    {
+        content:"Do the laundry"
+    },
+    {
+        content:"Study for the exam"
+    },
+    {
+        content:"Clean the house"
+    }
+])
